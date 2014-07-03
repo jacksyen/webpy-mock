8a17bceb35933bb87ff1fd23241fea2c83b2d609
jacksyen <hyqiu.syen@gmail.com>
2014-07-03 18:13:38 +0800

    add webglobal.py

---
+++ webglobal.py
@@ -0,0 +1,9 @@
+# -*- coding:utf-8 -*-
+
+class Global:
+
+    # 请求集合{'外部流水号': {easyLifeOrderNo:'', outBizNo: '', status:'', paymentType: '', userCode:'', paymentAmount:1.00}, '':{}}
+    GLOBAL_REQUEST = {}
+
+    def __init__(self):
+        pass
